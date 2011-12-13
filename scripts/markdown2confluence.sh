178c5f4d710ae6299ed492be776e35bd725aa16d
rhiokim <tizie@kthcorp.com>
2011-12-13 12:06:50 +0900

    add clipboard copy

--- scripts/markdown2confluence.sh
+++ scripts/markdown2confluence.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-echo $0
-echo $1
-echo $2
+argc=$#
+
+if [ 0 -eq $argc ]; then
+   echo "파라미터로 markdown 파일을 지정해주세요."
+   echo " usage>  md2conf example.markdown"
+   echo "                            "
+   echo " # clipboard copy option"
+   echo " ex>  md2conf example.markdown -c"
+   exit
+fi
 
 python ~/.dotfiles/scripts/Markdown-Converter/markdown_converter.py $1
 
+if [ "$2" = "-c" ]; then
+   cat $1.'txt' | pbcopy
+fi
